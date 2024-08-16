240e2ab195be5efa3cf215c6853cf3b7e3e27b3b
nchi5517 <neelagreev.ch@gmail.com>
2024-08-16 14:30:10 -0700

    Update Program5.java
    not done yet

--- Program5.java
+++ Program5.java
@@ -1,7 +1,36 @@
 //import stuff here?
 
 //Your code here
+public class Program5{
+    public static void main(String[] args){
+    int royalMiles = 286;
+    int koopakingMiles = 412;
+    int pipeframeMiles = 361;
+    int badwagonMiles = 161; 
+    int royalGallons = 9;
+    int koopakingGallons = 40;
+    int pipeframeGallons = 18;
+    int badwagonGallons = 11;
+    double royalAverage = 0;
+    double koopakingAverage = 0;
+    double pipeframeAverage = 0;
+    double badwagonAverage = 0;
+    royalAverage =(double) royalMiles/royalGallons;
+    koopakingAverage =(double) koopakingMiles/koopakingGallons;
+    pipeframeAverage =(double) pipeframeMiles/pipeframeGallons;
+    badwagonAverage =(double) badwagonMiles/badwagonGallons;
+    
+    System.out.println("Mushroom Cup Rix Racer Average Miles/Per Gallon: ");
+    System.out.println("Royal averaged "+royalAverage);
+    
+    
 
+    
+
+
+
+ }
+}
 //Paste console output below:
 /*
 
