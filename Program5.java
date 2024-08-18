fc5a03efded3605e92f454fb6836e39a6a885172
nchi5517 <neelagreev.ch@gmail.com>
2024-08-18 13:17:30 -0700

    Update Program5.java
    finished program 5

--- Program5.java
+++ Program5.java
@@ -1,7 +1,11 @@
-//import stuff here?
 
+
+//import stuff here?
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 //Your code here
 public class Program5{
+     private static final DecimalFormat df = new DecimalFormat("0.0");
     public static void main(String[] args){
     int royalMiles = 286;
     int koopakingMiles = 412;
@@ -11,18 +15,39 @@ public class Program5{
     int koopakingGallons = 40;
     int pipeframeGallons = 18;
     int badwagonGallons = 11;
-    double royalAverage = 0;
-    double koopakingAverage = 0;
-    double pipeframeAverage = 0;
-    double badwagonAverage = 0;
+    double royalAverage = 0.0;
+    double koopakingAverage = 0.0;
+    double pipeframeAverage = 0.0;
+    double badwagonAverage = 0.0;
     royalAverage =(double) royalMiles/royalGallons;
     koopakingAverage =(double) koopakingMiles/koopakingGallons;
     pipeframeAverage =(double) pipeframeMiles/pipeframeGallons;
     badwagonAverage =(double) badwagonMiles/badwagonGallons;
     
     System.out.println("Mushroom Cup Rix Racer Average Miles/Per Gallon: ");
-    System.out.println("Royal averaged "+royalAverage);
+     df.setRoundingMode(RoundingMode.UP);
+    System.out.println("Royal averaged "+ df.format(royalAverage)+" m/g");
+     df.setRoundingMode(RoundingMode.UP);
+    System.out.println("Koopa King averaged "+ df.format(koopakingAverage)+" m/g");
+     df.setRoundingMode(RoundingMode.UP);
+    System.out.println("Pipe Frame averaged "+ df.format(pipeframeAverage)+" m/g");
+     df.setRoundingMode(RoundingMode.DOWN);
+    System.out.println("Badwagon averaged "+ df.format(badwagonAverage)+" m/g");
     
+    
+
+    
+
+
+
+ }
+}
+//Paste console output below:
+/*
+
+
+*/
+
     
 
     
